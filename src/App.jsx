64d422d2ef920d1224a7dfab4eef7fe5ebb2fb84
DiriARG <matidirisio@gmail.com>
2026-09-17 14:12:41 -0300

    Página de inicio --> Apartado "Hero".

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { PaginaPrincipal } from "./pages/PaginaPrincipal";
 import { PaginaNoEncontrada } from "./pages/NoEncontrado";
 import { Route, Routes } from "react-router";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<PaginaPrincipal />} />
       <Route path="*" element={<PaginaNoEncontrada />} />
     </Routes>
   );
---
+++ src/pages/PaginaPrincipal.jsx
@@ -0,0 +1,9 @@
+import { Hero } from "../components/public/Hero";
+export function PaginaPrincipal() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
+}
+export default PaginaPrincipal;
