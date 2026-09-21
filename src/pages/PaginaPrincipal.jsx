ea3f2f58f53a2747ae009ff7e38118b708bd1028
DiriARG <matidirisio@gmail.com>
2026-09-21 20:40:49 -0300

    Página de inicio --> Footer.

--- src/pages/PaginaPrincipal.jsx
+++ src/pages/PaginaPrincipal.jsx
@@ -3,6 +3,7 @@ import { PorqueElegirnos } from "../components/publico/PorqueElegirnos";
 import { Nosotros } from "../components/publico/Nosotros";
 import { Planes } from "../components/publico/Planes";
 import { Contacto } from "../components/publico/Contacto";
+import { Footer } from "../components/publico/Footer";
 
 export function PaginaPrincipal() {
   return (
@@ -12,6 +13,7 @@ export function PaginaPrincipal() {
       <Nosotros />
       <Planes />
       <Contacto />
+      <Footer />
     </main>
   );
 }
