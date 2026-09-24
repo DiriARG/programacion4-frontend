700b994601d348db6eca89e122ee8547a69a44a1
DiriARG <matidirisio@gmail.com>
2026-09-24 17:25:34 -0300

    Implementación de componentes "RutaPrivada" y "RutaPublica" para control de acceso.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { Route, Routes } from "react-router";
+
+import { RutaPrivada } from "./routes/RutaPrivada";
+import { RutaPublica } from "./routes/RutaPublica";
 import { PaginaPrincipal } from "./pages/PaginaPrincipal";
 import { PaginaLogin } from "./pages/PaginaLogin";
 import { PaginaNoEncontrada } from "./pages/NoEncontrado";
-import { Route, Routes } from "react-router";
 
 function App() {
   return (
     <Routes>
+      {/* Página principal accesible con o sin sesión. */}
       <Route path="/" element={<PaginaPrincipal />} />
-      <Route path="/login" element={<PaginaLogin />} />
+
+      <Route element={<RutaPublica />}>
+        <Route path="/login" element={<PaginaLogin />} />
+      </Route>
+
+      <Route element={<RutaPrivada />}></Route>
+
       <Route path="*" element={<PaginaNoEncontrada />} />
     </Routes>
   );
